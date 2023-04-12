7ee440f309f4c866717527aba47c490e751540fe
Kai <yikai807809@gmail.com>
2023-04-12 10:51:18 +0800

    4/12 更新

--- src/main/java/com/ezticket/web/users/controller/RoleauthorityController.java
+++ src/main/java/com/ezticket/web/users/controller/RoleauthorityController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/back-users-my-team")
+@RequestMapping("/roleauthority")
 public class RoleauthorityController {
 
     @Autowired
--- src/main/java/com/ezticket/web/users/pojo/Member.java
+++ src/main/java/com/ezticket/web/users/pojo/Member.java
@@ -1,5 +1,6 @@
 package com.ezticket.web.users.pojo;
 
+import com.ezticket.core.pojo.Core;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +13,7 @@ import java.sql.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "MEMBER")
-public class Member {
+public class Member extends Core {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
