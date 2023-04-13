a8d83654ae2e594b43c8b740057a8079a370cf76
liang0731 <liangjie0731@gmail.com>
2023-04-14 01:57:00 +0800

    新增前台會員領取優惠卷區塊

--- src/main/resources/static/js/product-profile_view.js
+++ src/main/resources/static/js/product-profile_view.js
@@ -10,6 +10,7 @@ $(document).ready(async () => {
   if(memberno){
     fetchPorderList(`/porder/getordersbyid?id=${memberno}`);
     fetchPcouponHoldingList(`/pcouponholding/byMemberno?memberno=${memberno}`);
+    fetchPcouponList(`/pcoupon/list`);
   } else {
     // 請前往登入
     window.location.href = 'front-users-mem-sign-in.html';
@@ -145,7 +146,8 @@ function fetchPcouponHoldingList(e) {
     method: 'GET',
   }).then(response => response.json())
     .then(data => {
-      const followers = document.querySelector('#coupons');
+      data.reverse();
+      const followers = document.querySelector('#coupons-01');
       followers.innerHTML = '';
       const couponHolding = data.map(obj => {
         return fetch(`/pcoupon?id=${obj.pcouponholdingPK.pcouponno}`, {
@@ -195,4 +197,80 @@ function fetchPcouponHoldingList(e) {
         followers.innerHTML = values.join('');
       });
     });
+}
+
+function fetchPcouponList(e) {
+	fetch(e, {
+		method: 'GET',
+	}).then(response => response.json())
+		.then(data => {
+			data.reverse();
+			const couponlist = document.querySelector('#coupons-02');
+			couponlist.innerHTML = "";
+			const couponbody = data.map(obj => {
+				return `<div class="main-card mt-4">
+                  <div class="contact-list coupon-active">
+                    <div class="top d-flex flex-wrap justify-content-between align-items-center p-4 border_bottom">
+                      <div class="icon-box">
+                        <span class="icon-big rotate-icon icon icon-purple">
+                          <i class="fa-solid fa-ticket"></i>
+                        </span>
+                        <h5 class="font-18 mb-1 mt-1 f-weight-medium">${obj.pcouponno}<span class="font-weight-normal"> -
+                        ${obj.pcouponname}</span></h5>
+                        <p class="text-gray-50 m-0"><span class="visitor-date-time">${moment(obj.pcoupnsdate).format('YYYY-MM-DD HH:mm:ss')}
+                          </span> - <span class="visitor-date-time">${moment(obj.pcoupnedate).format('YYYY-MM-DD HH:mm:ss')}</span></p>
+                      </div>
+                      <div class="d-flex align-items-center">
+                        <label class="btn-switch tfs-8 mb-0 me-4 mt-1">
+                        <input type="checkbox" onchange="changeStatus(this)" value="" ${obj.pcouponstatus === 1 ? 'checked' : 'false'}>
+                          <span class="checkbox-slider"></span>
+                        </label>
+                        <div class="dropdown dropdown-default dropdown-text dropdown-icon-item">
+                          <button class="option-btn-1" type="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                            <a href="#" class="dropdown-item" data-bs-toggle="modal"
+														data-bs-target="#editcouponModal" onclick="editCoupon(${obj.pcouponno})"><i class="fa-solid fa-pen me-3"></i>編輯</a>
+                            <a href="#" class="dropdown-item" onclick="saveCoupon(${obj.pcouponno}, event)"><i class="fa-solid fa-floppy-disk me-3"></i>儲存</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="bottom d-flex flex-wrap justify-content-between align-items-center p-4">
+                      <div class="icon-box ">
+                        <span class="icon">
+                          <i class="fa-regular fa-circle-dot"></i>
+                        </span>
+                        <p>狀態</p>
+                        <h6 class="coupon-status">${obj.pcouponstatus === 1 ? '可使用' : '不可使用'}</h6>
+                      </div>
+                      <div class="icon-box">
+                        <span class="icon">
+                          <i class="fa-solid fa-tag"></i>
+                        </span>
+                        <p>適用商品編號</p>
+                        <h6 class="coupon-status">${obj.pfitcoupons && obj.pfitcoupons[0] && obj.pfitcoupons[0].pfitcouponNo.productno}</h6>
+                      </div>
+                      <div class="icon-box">
+                        <span class="icon">
+                          <i class="fa-solid fa-money-bill"></i>
+                        </span>
+                        <p>消費金額</p>
+                        <h6 class="coupon-status">${obj.preachprice}</h6>
+                      </div>
+                      <div class="icon-box">
+                        <span class="icon">
+                          <i class="fa-solid fa-money-bill"></i>
+                        </span>
+                        <p>折扣</p>
+                        <h6 class="coupon-status">${obj.pdiscount}</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+			}).join('');
+			couponlist.innerHTML += couponbody;
+		})
 }
