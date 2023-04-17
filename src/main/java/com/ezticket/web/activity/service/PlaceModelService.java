149dfcb69db564b2d6c489bc8523309a28ed8fe5
Melody Huang <yth.melody@gmail.com>
2023-04-17 16:30:30 +0800

    Update Place and BlockModel service

--- src/main/java/com/ezticket/web/activity/service/PlaceModelService.java
+++ src/main/java/com/ezticket/web/activity/service/PlaceModelService.java
@@ -55,13 +55,13 @@ public class PlaceModelService {
     //    新增/修改模板
     @Transactional
     public PlaceModelDTO savePlaceModel(PlaceModelDTO placeModelDTO) {
-        PlaceModel placeModel;
-        if (placeModelDTO.getModelno()!=null) {
-            //      findById 有找到修改、沒找到新增
-            placeModel = placeModelRepository.findById(placeModelDTO.getModelno()).orElse(new PlaceModel());
-        } else {
-            placeModel = new PlaceModel();
-        }
+        PlaceModel placeModel = new PlaceModel();
+//        if (placeModelDTO.getModelno()!=null) {
+//            //      findById 有找到修改、沒找到新增
+//            placeModel = placeModelRepository.findById(placeModelDTO.getModelno()).orElse(new PlaceModel());
+//        } else {
+//            placeModel = new PlaceModel();
+//        }
         BeanUtils.copyProperties(placeModelDTO, placeModel);
         PlaceModel savedPlaceModel = placeModelRepository.save(placeModel);
         BeanUtils.copyProperties(savedPlaceModel, placeModelDTO);
